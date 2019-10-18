c67b0df22ead27a51b1edc66670c5a7e01020ac3
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-18 16:40:41 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-favorite_number = 3
+first_number = 3
+second_number = 4
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
